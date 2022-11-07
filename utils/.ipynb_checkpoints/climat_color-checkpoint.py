c2b73fae4916153828657de4572ea16cfc6104ef
JulienD <julien.duthoit0@gmail.com>
2022-11-07 19:14:27 +0100

    initial commit

---
+++ utils/.ipynb_checkpoints/climat_color-checkpoint.py
@@ -0,0 +1,6 @@
+climat_color={'océanique NO': "blue", 
+              'océanique dégradé': "green", 
+              'semi-continental': "olive",
+              'océanique aquitain': "purple", 
+              'montagne': "brown", 
+              'méditerranéen': "orange"}
