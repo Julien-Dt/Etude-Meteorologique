62d1b625de7d0b6e390aa82ca36f326af6876b6d
JulienD <julien.duthoit0@gmail.com>
2022-11-08 13:25:38 +0100

    suppression de fichiers inutiles

--- utils/.ipynb_checkpoints/climat_color-checkpoint.py
+++
@@ -1,6 +0,0 @@
-climat_color={'océanique NO': "blue", 
-              'océanique dégradé': "green", 
-              'semi-continental': "olive",
-              'océanique aquitain': "purple", 
-              'montagne': "brown", 
-              'méditerranéen': "orange"}
